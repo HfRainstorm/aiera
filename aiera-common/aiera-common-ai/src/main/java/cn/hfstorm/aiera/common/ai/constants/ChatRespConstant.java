907812363a89908d1a2a9c3b99cbf66881b11adb
HfRainstorm <hmy_hammer@163.com>
2025-02-08 10:28:13 +0800

    resolve dependencies

---
+++ aiera-common/aiera-common-ai/src/main/java/cn/hfstorm/aiera/common/ai/constants/ChatRespConstant.java
@@ -0,0 +1,14 @@
+package cn.hfstorm.aiera.common.ai.constants;
+
+/**
+ * chat resp constant
+ * @author : hmy
+ * @date : 2025/2/8 10:25
+ */
+public class ChatRespConstant {
+
+    /**
+     * content concat end with
+     */
+    public static final String CONTENT_CONCAT_END_WITH = "[complete]";
+}
